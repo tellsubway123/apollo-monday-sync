9a0611af8845e3256dac9af99a8006d4574fa543
tellsubway123 <louis.ruiz@timetocollide.com>
2026-08-07 17:48:53 -0400

    Update API endpoint and response print limit
    Limit printed response text to the first 4000 characters.

--- sync.py
+++ sync.py
@@ -4,11 +4,11 @@ import requests
 APOLLO_API_KEY = os.environ["APOLLO_API_KEY"]
 
 response = requests.get(
-    "https://api.apollo.io/api/v1/people/6a75e1f39598d700146166f3",
+    "https://api.apollo.io/api/v1/contacts/6a75e1f39598d700146166f3",
     headers={
         "X-Api-Key": APOLLO_API_KEY
     }
 )
 
 print("Status:", response.status_code)
-print(response.text)
+print(response.text[:4000])
