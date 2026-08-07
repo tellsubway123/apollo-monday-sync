8028b21f8bca43fdb85fd99256d5a07ec3db5a3c
tellsubway123 <louis.ruiz@timetocollide.com>
2026-08-07 17:55:04 -0400

    Fix Content-Type header in API request

--- sync.py
+++ sync.py
@@ -7,4 +7,15 @@ response = requests.post(
     "https://api.apollo.io/api/v1/contacts/search",
     headers={
         "X-Api-Key": APOLLO_API_KEY,
-        "Content-Type": "application
+        "Content-Type": "application/json"
+    },
+    json={
+        "page": 1,
+        "per_page": 10,
+        "sort_by_field": "created_at",
+        "sort_ascending": False
+    }
+)
+
+print("Status:", response.status_code)
+print(response.text[:4000])
