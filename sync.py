26b5ed8a22f2047e3a0984efef9519e7a433768a
tellsubway123 <louis.ruiz@timetocollide.com>
2026-08-07 17:42:28 -0400

    Update API endpoint in sync.py

--- sync.py
+++ sync.py
@@ -4,7 +4,7 @@ import requests
 APOLLO_API_KEY = os.environ["APOLLO_API_KEY"]
 
 response = requests.get(
-    "https://api.apollo.io/api/v1/people/59fe56a7a6da9861955e1ec1",
+    "https://api.apollo.io/api/v1/people/6a75e1f39598d700146166f3",
     headers={
         "X-Api-Key": APOLLO_API_KEY
     }
