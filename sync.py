57cbc9cb6d481a0c77ea2fc562b106dffc7af5c8
tellsubway123 <louis.ruiz@timetocollide.com>
2026-08-07 12:54:04 -0400

    Add print statements for Board ID and User ID

--- sync.py
+++ sync.py
@@ -2,3 +2,5 @@ import os
 
 print("Apollo API Key Found:", bool(os.environ.get("APOLLO_API_KEY")))
 print("Monday API Token Found:", bool(os.environ.get("MONDAY_API_TOKEN")))
+print("Board ID: 18395580962")
+print("Monday User ID: 103624857")
