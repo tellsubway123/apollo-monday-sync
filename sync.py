b941a0048a5434eb97a0eebc812149067829ce37
tellsubway123 <louis.ruiz@timetocollide.com>
2026-08-07 17:53:42 -0400

    Refactor API request by removing JSON payload
    Removed unnecessary JSON payload and printing of contacts.

--- sync.py
+++ sync.py
@@ -7,18 +7,4 @@ response = requests.post(
     "https://api.apollo.io/api/v1/contacts/search",
     headers={
         "X-Api-Key": APOLLO_API_KEY,
-        "Content-Type": "application/json"
-    },
-    json={
-        "page": 1,
-        "per_page": 5
-    }
-)
-
-data = response.json()
-
-for contact in data.get("contacts", []):
-    print("NAME:", contact.get("name"))
-    print("ID:", contact.get("id"))
-    print("CREATED:", contact.get("created_at"))
-    print("---")
+        "Content-Type": "application
