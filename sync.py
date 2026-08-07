610f6b9756cc9540deb0dbe65a2e25e9702a714e
tellsubway123 <louis.ruiz@timetocollide.com>
2026-08-07 17:50:14 -0400

    Change API request from GET to POST

--- sync.py
+++ sync.py
@@ -3,10 +3,15 @@ import requests
 
 APOLLO_API_KEY = os.environ["APOLLO_API_KEY"]
 
-response = requests.get(
-    "https://api.apollo.io/api/v1/contacts/6a75e1f39598d700146166f3",
+response = requests.post(
+    "https://api.apollo.io/api/v1/contacts/search",
     headers={
-        "X-Api-Key": APOLLO_API_KEY
+        "X-Api-Key": APOLLO_API_KEY,
+        "Content-Type": "application/json"
+    },
+    json={
+        "page": 1,
+        "per_page": 5
     }
 )
 
