77bb51ab686aabbf4146cf2678a66cabeb1f04df
tellsubway123 <louis.ruiz@timetocollide.com>
2026-08-10 18:15:04 -0400

    Integrate Apollo API to fetch contact details
    Removed print statements and added API call to fetch contacts.

--- sync.py
+++ sync.py
@@ -1,5 +1,23 @@
-print("TOP")
+import os
+import requests
 
-print("MIDDLE")
+APOLLO_API_KEY = os.environ["APOLLO_API_KEY"]
 
-print("BOTTOM")
+response = requests.post(
+    "https://api.apollo.io/api/v1/contacts/search",
+    headers={
+        "X-Api-Key": APOLLO_API_KEY,
+        "Content-Type": "application/json"
+    },
+    json={
+        "page": 1,
+        "per_page": 1,
+        "sort_by_field": "created_at",
+        "sort_ascending": False
+    }
+)
+
+contact = response.json()["contacts"][0]
+
+print("NAME:", contact.get("name"))
+print("EMAIL:", contact.get("email"))
