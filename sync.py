fd1f7a92cc6064d0d3d825e7b53e3fa1e02fedca
tellsubway123 <louis.ruiz@timetocollide.com>
2026-08-11 13:51:19 -0400

    Refactor sync.py to remove API call
    Removed API call to Apollo and added file reading for processed contacts.

--- sync.py
+++ sync.py
@@ -1,20 +1,11 @@
 import os
 import json
-import requests
 
-APOLLO_API_KEY = os.environ["APOLLO_API_KEY"]
+PROCESSED_FILE = "processed_contacts.json"
 
-response = requests.post(
-    "https://api.apollo.io/api/v1/contacts/search",
-    headers={
-        "X-Api-Key": APOLLO_API_KEY,
-        "Content-Type": "application/json"
-    },
-    json={
-        "owner_ids": ["6a171a990217cf001039ff2a"],
-        "page": 1,
-        "per_page": 20
-    }
-)
+with open(PROCESSED_FILE, "r") as f:
+    processed_contacts = json.load(f)
 
-print(json.dumps(response.json(), indent=2)[:5000])
+print("PROCESSED CONTACTS:")
+print(processed_contacts)
+print("COUNT:", len(processed_contacts))
