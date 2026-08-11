9d69873585cdbba1fd388edbde8e49e8d1f142dc
tellsubway123 <louis.ruiz@timetocollide.com>
2026-08-11 14:08:20 -0400

    Update sync.py to read and process contacts
    Refactor sync.py to read processed contacts from a file and send a request to Apollo API for new contacts.

--- sync.py
+++ sync.py
@@ -1,15 +1,42 @@
+import os
 import json
+import requests
+
+APOLLO_API_KEY = os.environ["APOLLO_API_KEY"]
 
 PROCESSED_FILE = "processed_contacts.json"
 
-with open(PROCESSED_FILE, "w") as f:
-    json.dump(
-        [
-            "test-id-1",
-            "test-id-2"
-        ],
-        f,
-        indent=2
-    )
+with open(PROCESSED_FILE, "r") as f:
+    processed_contacts = json.load(f)
 
-print("FILE UPDATED")
+response = requests.post(
+    "https://api.apollo.io/api/v1/contacts/search",
+    headers={
+        "X-Api-Key": APOLLO_API_KEY,
+        "Content-Type": "application/json"
+    },
+    json={
+        "owner_ids": ["6a171a990217cf001039ff2a"],
+        "page": 1,
+        "per_page": 20
+    }
+)
+
+contacts = response.json().get("contacts", [])
+
+new_count = 0
+
+for contact in contacts:
+
+    contact_id = contact.get("id")
+    name = contact.get("name")
+
+    if contact_id in processed_contacts:
+
+        print("SKIP |", name)
+
+    else:
+
+        print("NEW  |", name)
+
+        processed_contacts.append
