c54c2c19c4cfafa12a076815ebc03c5ed6ad3821
tellsubway123 <louis.ruiz@timetocollide.com>
2026-08-11 11:11:06 -0400

    Multi-field update test
    Added OWNER_ID and included company and mobile fields in the request payload.

--- sync.py
+++ sync.py
@@ -6,6 +6,7 @@ APOLLO_API_KEY = os.environ["APOLLO_API_KEY"]
 MONDAY_API_TOKEN = os.environ["MONDAY_API_TOKEN"]
 
 BOARD_ID = 18395580962
+OWNER_ID = 103624857
 
 response = requests.post(
     "https://api.apollo.io/api/v1/contacts/search",
@@ -25,6 +26,8 @@ contact = response.json()["contacts"][0]
 
 email = contact.get("email", "")
 title = contact.get("title", "")
+company = contact.get("organization_name", "")
+mobile = contact.get("sanitized_phone", "")
 
 search_query = f"""
 query {{
@@ -69,7 +72,24 @@ if items:
     item_id = items[0]["id"]
 
     values = {
-        "text_mkzmfmqb": title
+        "text_mkzmfmqb": title,
+        "text_mkzm1fns": company,
+        "email_mm47srsd": {
+            "email": email,
+            "text": email
+        },
+        "phone_mkzmcmj7": {
+            "phone": mobile,
+            "countryShortName": "US"
+        },
+        "multiple_person_mm16b6ej": {
+            "personsAndTeams": [
+                {
+                    "id": OWNER_ID,
+                    "kind": "person"
+                }
+            ]
+        }
     }
 
     mutation = """
