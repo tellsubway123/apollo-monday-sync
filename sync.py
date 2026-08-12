2bd95bb800ad7ee4d8b8f05da1167e05b7c549d9
tellsubway123 <louis.ruiz@timetocollide.com>
2026-08-12 12:09:30 -0400

    Modify API request to search contacts
    Change API request from GET to POST and update payload.

--- sync.py
+++ sync.py
@@ -1,14 +1,28 @@
 import os
+import json
 import requests
 
 APOLLO_API_KEY = os.environ["APOLLO_API_KEY"]
 
-response = requests.get(
-    "https://api.apollo.io/api/v1/emailer_campaigns",
+response = requests.post(
+    "https://api.apollo.io/api/v1/contacts/search",
     headers={
-        "X-Api-Key": APOLLO_API_KEY
+        "X-Api-Key": APOLLO_API_KEY,
+        "Content-Type": "application/json"
+    },
+    json={
+        "emailer_campaign_ids": [
+            "6a60cc79d0d4430020e6e356",
+            "6a60cd15302179001cdd84e4"
+        ],
+        "page": 1,
+        "per_page": 100
     }
 )
 
-print(response.status_code)
-print(response.text[:5000])
+data = response.json()
+
+print("CONTACT COUNT:", len(data.get("contacts", [])))
+
+for contact in data.get("contacts", []):
+    print(contact.get("name"))
