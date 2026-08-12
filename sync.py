70ecc404871a0a32ae2efa7623492891ee6a02c5
tellsubway123 <louis.ruiz@timetocollide.com>
2026-08-12 10:20:42 -0400

    Store real Apollo IDs
    Updated the script to fetch contacts from Apollo API and save processed contact IDs.

--- sync.py
+++ sync.py
@@ -1,16 +1,50 @@
+import os
 import json
+import requests
+
+APOLLO_API_KEY = os.environ["APOLLO_API_KEY"]
 
 PROCESSED_FILE = "processed_contacts.json"
 
-with open(PROCESSED_FILE, "w") as f:
-    json.dump(
-        [
-            "apollo-test-1",
-            "apollo-test-2",
-            "apollo-test-3"
-        ],
-        f,
-        indent=2
-    )
+with open(PROCESSED_FILE, "r") as f:
+    processed_contacts = json.load(f)
 
-print("WROTE TEST IDS")
+response = requests.post(
+    "https://api.apollo.io/api/v1/contacts/search",
+    headers={
+        "X-Api-Key": APOLLO_API_KEY,
+        "Content-Type": "application/json"
+    },
+    json={
+        "owner_ids": ["6a171a990217cf001039ff2a"],
+        "page": 1,
+        "per_page": 20
+    }
+)
+
+contacts = response.json().get("contacts", [])
+
+new_count = 0
+
+for contact in contacts:
+
+    contact_id = contact.get("id")
+    name = contact.get("name")
+
+    if contact_id in processed_contacts:
+
+        print("SKIP |", name)
+
+    else:
+
+        print("NEW  |", name)
+
+        processed_contacts.append(contact_id)
+        new_count += 1
+
+with open(PROCESSED_FILE, "w") as f:
+    json.dump(processed_contacts, f, indent=2)
+
+print()
+print("NEW CONTACTS:", new_count)
+print("TOTAL IDS:", len(processed_contacts))
